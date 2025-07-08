6323dd431c138267e98e320ab51471367f26587a
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-08 13:45:31 +0530

    Added initial routing fils for auth and root

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 
-const page = () => {
+const HomePage = () => {
   return (
     <div>
       Next Interview Trainer.
@@ -7,4 +7,4 @@ const page = () => {
   )
 }
 
-export default page
+export default HomePage
