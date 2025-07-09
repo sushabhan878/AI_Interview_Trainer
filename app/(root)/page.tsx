b36ce9d55928561be5656080d32aa31dcedf8097
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-09 20:57:25 +0530

    Implimented the home banner

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,35 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import Link from "next/link"
 
 const HomePage = () => {
   return (
-    <div>
-      Next Interview Trainer.
-    </div>
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview redy with AI-Powered Practice & Feedback</h2>
+          <p className="lext-lg">
+            practice on real interview questions & get instant feedback from AI
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+        <Image src="/robot.png" alt="robot" width={400} height={400} className="max-sm:hidden" />
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          <p>You haven't taken any interviews yet</p>
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          <p>There are no interviews available</p>
+        </div>
+      </section>
+    </>
   )
 }
 
