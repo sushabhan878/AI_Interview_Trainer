1fb22fdc9f2ead28be2d45677dbd74b77f9923cd
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-09 21:47:02 +0530

    Added lib assets, created the interview card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import InterviewCard from "@/components/InterviewCard"
 import { Button } from "@/components/ui/button"
+import { dummyInterviews } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -20,13 +22,19 @@ const HomePage = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-          <p>You haven't taken any interviews yet</p>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+          {/* <p>You haven't taken any interviews yet</p> */}
         </div>
       </section>
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
         <div className="interviews-section">
-          <p>There are no interviews available</p>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+          {/* <p>There are no interviews available</p> */}
         </div>
       </section>
     </>
