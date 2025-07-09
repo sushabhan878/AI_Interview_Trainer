6c04347c5d4979c457afbd57c95a1202e4a33f82
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-09 12:22:57 +0530

    Working on auth forms

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const SignUpPage = () => {
     return (
         <div>
-            <AuthForm type="sign-in" />
+            <AuthForm type="sign-up" />
         </div>
     )
 }
