e68578915cb6f169e3a9a345602d73ddbe920066
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-10 12:37:09 +0530

    sign up done

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -11,6 +11,9 @@ import Link from "next/link"
 import { toast } from "sonner"
 import FormField from "./FormField"
 import { useRouter } from "next/navigation"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from "@/firebase/client"
+import { signUp } from "@/lib/actions/auth.action"
 
 const authFormSchema = (type: FormType) => {
     return z.object({
@@ -33,9 +36,21 @@ const AuthForm = ({ type }: { type: FormType }) => {
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    async function onSubmit(values: z.infer<typeof formSchema>) {
         try {
             if (type === "sign-up") {
+                const { name, email, password } = values
+                const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+                const result = await signUp({
+                    uid: userCredentials.user.uid,
+                    name: name!,
+                    email,
+                    password
+                })
+                if (!result?.success) {
+                    toast.error(result?.message)
+                    return
+                }
                 toast.success("Account created successfully, Please sign in and start preparing for interviews with the help of AI")
                 router.push("/sign-in")
             } else {
